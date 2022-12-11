25ca54ab8fc30b17296085caedd02b3579af4192
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-12-12 00:28:31 +0530

    Project created and database created

---
+++ MyStore/Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        public List<ClientInfo> listClients = new List<ClientInfo>();
+        public void OnGet()
+        {
+           try
+            {
+                String connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=mystore;Integrated Security=True";
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "select * from clients ;";
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read()) {
+                            ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at=reader.GetDateTime(5).ToString();
+
+                                listClients.Add(clientInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception" +ex.Message);
+            }
+        }
+    }
+
+    public class ClientInfo
+    {
+        public String id;
+        public String name;
+        public String email;
+        public String phone;
+        public String address;
+        public String created_at;
+
+    }
+}
